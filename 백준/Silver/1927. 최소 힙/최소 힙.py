e8701eeb477bdeae1c199a20838b4a5408a1988d
hellfer <147736877+hellfer@users.noreply.github.com>
2023-10-19 19:20:39 +0900

    [Silver II] Title: 최소 힙, Time: 120 ms, Memory: 37044 KB -BaekjoonHub

--- 백준/Silver/1927. 최소 힙/최소 힙.py
+++ 백준/Silver/1927. 최소 힙/최소 힙.py
@@ -1,18 +1,16 @@
-from queue import PriorityQueue
+import heapq
 import sys
 
 n = int(sys.stdin.readline())
-queue = PriorityQueue()
 result = []
 
 for _ in range(n):
     x = int(sys.stdin.readline())
-    result.append(x)
-    
+
     if x == 0:
-        if queue.empty():
+        if not result:  # result가 비어있으면
             print(0)
         else:
-            print(queue.get())
+            print(heapq.heappop(result))
     else:
-        queue.put(x)
+        heapq.heappush(result, x)

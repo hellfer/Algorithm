f4bf0951bd89b07325ad18ab094d371555a29a92
hellfer <147736877+hellfer@users.noreply.github.com>
2023-10-25 19:51:14 +0900

    [Silver IV] Title: 에라토스테네스의 체, Time: 52 ms, Memory: 33240 KB -BaekjoonHub

---
+++ 백준/Silver/2960. 에라토스테네스의 체/에라토스테네스의 체.py
@@ -0,0 +1,19 @@
+import math
+
+n, k = map(int, input().split())
+array = [True for i in range(n + 1)]
+result = []
+for i in range(2, n + 1):
+    if array[i] == True:
+        for j in range(i, n + 1, i):
+            if array[j] == True:
+                array[j] = False
+                result.append(j)
+                if len(result) == k:
+                    print(result[-1])
+                    break
+        if len(result) == k:
+            break
+
+
+

6706c73f786844dcdf61adab70b9abc714cd5784
hellfer <147736877+hellfer@users.noreply.github.com>
2023-10-26 22:20:27 +0900

    [Silver II] Title: 베르트랑 공준, Time: 2108 ms, Memory: 36480 KB -BaekjoonHub

--- 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.py
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.py
@@ -7,7 +7,7 @@ while True:
     array = [True for i in range(2 * n + 1)]
     for i in range(2, int((2 * n) ** 0.5) + 1):
         if array[i]:
-            for j in range(i + i, 2 * n + 1, i):
+            for j in range(i, 2 * n + 1, i):
                 array[j] = False
     print(len([i for i in range(n + 1, 2 * n + 1) if array[i]]))
 

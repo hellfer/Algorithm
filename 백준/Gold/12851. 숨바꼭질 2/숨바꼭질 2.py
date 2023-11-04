b8b27e8b2b08cc402611273bef60016ff71c860c
JEONG JU <147736877+hellfer@users.noreply.github.com>
2023-11-04 20:54:06 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 116 ms, Memory: 36464 KB -BaekjoonHub

--- 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.py
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.py
@@ -1,31 +1,28 @@
 from collections import deque
 
 def bfs():
-    q = deque()
-    q.append(n)
-    time[n] = 0
-    cnt[n] = 1
+    queue=deque()
+    queue.append(n)
+    visited[n]=0
+    count[n]=1
+    while queue:
+        x=queue.popleft()
+        for nx in [x-1,x+1,x*2]:
+            if 0<=nx<100001:
+                if visited[nx]==-1:
+                    visited[nx]=visited[x]+1
+                    count[nx]=count[x]
+                    queue.append(nx)
+                elif visited[nx]==visited[x]+1:
+                    count[nx]+=count[x]
 
-    while q:
-        x = q.popleft()
-        for nx in (x-1, x+1, x*2):
-            if 0 <= nx < 100001:
-                if time[nx] == -1:
-                    time[nx] = time[x] + 1
-                    cnt[nx] = cnt[x]
-                    q.append(nx)
-                elif time[nx] == time[x] + 1:
-                    cnt[nx] += cnt[x]
-
-n, k = map(int, input().split())
-time = [-1]*100001
-cnt = [0]*100001
-
+                    
+n,m=map(int,input().split())
+visited=[-1]*100001
+count=[0]*100001
 bfs()
-
-print(time[k])
-print(cnt[k])
-
+print(visited[m])
+print(count[m])
 
 
 

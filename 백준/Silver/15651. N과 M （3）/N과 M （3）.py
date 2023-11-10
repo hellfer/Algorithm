dab3346022fa40ddfe0f54dfe09ec353f413c83f
JEONG JU <147736877+hellfer@users.noreply.github.com>
2023-11-10 14:39:17 +0900

    [Silver III] Title: N과 M (3), Time: 1356 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.py
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -1,21 +1,13 @@
-def backtracking(n, m, visited, result):
+def backtracking(n, m, result):
     if len(result) == m:
         print(' '.join(map(str, result)))
         return
     
     for i in range(1, n+1):
-        if visited[i]:
-            continue
-            
-        visited[i] = False
-        result.append(i)
-        
-        backtracking(n, m, visited, result)
-        
-        
+        result.append(i)      
+        backtracking(n, m, result)     
         result.pop()
         
 n, m = map(int, input().split())
-visited = [False] * (n+1)
 result = []
-backtracking(n, m, visited, result)
+backtracking(n, m, result)

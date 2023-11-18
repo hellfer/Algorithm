c736aa26e0806f04c49ec9ddcb6415a435be1fd8
JEONG JU <147736877+hellfer@users.noreply.github.com>
2023-11-18 14:04:53 +0900

    [Silver IV] Title: 설탕 배달, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -1,12 +1,11 @@
-N = int(input())
-bag = 0
-
-while N >= 0 :
-    if N % 5 == 0 :  # 5로 나누어 떨어지면
-        bag += (N // 5)  # 5로 나눈 몫을 봉지 개수에 추가
-        print(bag)
+n = int(input())
+result = 0
+while n>=0:
+    if n%5==0:
+        result+=n//5
+        print(result)
         break
-    N -= 3  # 5의 배수가 될 때까지 설탕 무게에서 3을 빼줌
-    bag += 1  # 3킬로그램 봉지 추가
-else :
+    n-=3
+    result+=1
+else:
     print(-1)

c6334fbf17f634da2718118c72b27f0281ddfb91
JEONG JU <147736877+hellfer@users.noreply.github.com>
2023-11-22 19:00:29 +0900

    [Gold V] Title: 암호 만들기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.py
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.py
@@ -12,12 +12,12 @@ def check(password):
     return mo >= 1 and ja >= 2
 
 # 입력을 받습니다.
-L, C = map(int, input().split())
+n, m = map(int, input().split())
 array = input().split()
 array.sort()
 
 # 가능한 모든 경우의 수를 조합을 이용해서 만듭니다.
-for password in combinations(array, L):
+for password in combinations(array, n):
     # 만약 자음 모음 검사를 통과한다면 출력합니다.
     if check(password):
         print(''.join(password))

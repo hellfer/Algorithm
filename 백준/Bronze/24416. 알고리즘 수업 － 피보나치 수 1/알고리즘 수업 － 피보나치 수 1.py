f82387714a290d4cab1b2a8379eafa147b59cd84
JEONG JU <147736877+hellfer@users.noreply.github.com>
2023-12-18 13:00:27 +0900

    [Bronze I] Title: 알고리즘 수업 - 피보나치 수 1, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.py
+++ 백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.py
@@ -1,23 +1,16 @@
 def fibo(n):
-    dp = [0] * (n + 1)  # 계산 결과를 저장할 리스트
-
-    # 기저 조건 초기화
-    dp[0] = 0
-    dp[1] = 1
-
-    # 피보나치 수열 계산
-    for i in range(2, n + 1):
-        dp[i] = dp[i - 1] + dp[i - 2]
-
+    dp=[0]*(n+1)
+    dp[0]=0
+    dp[1]=1
+    
+    for i in range(2,n+1):
+        dp[i]=dp[i-1]+dp[i-2]
     return dp[n]
 
-n = int(input())
+n=int(input())
 
-x = fibo(n)
+x=fibo(n)
 
-y = n - 2
+y=n-2
 
-print(x, y)
-
-
-
+print(x,y)

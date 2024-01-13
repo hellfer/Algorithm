af0421704baceb96572735f4831af4cf20e3fc65
JEONG JU <147736877+hellfer@users.noreply.github.com>
2024-01-13 22:19:14 +0900

    [Gold V] Title: 빗물, Time: 116 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Gold/14719. 빗물/빗물.py
+++ 백준/Gold/14719. 빗물/빗물.py
@@ -1,20 +1,18 @@
-H, W = map(int, input().split())
-blocks = list(map(int, input().split()))
+n,m=map(int,input().split())
+x=list(map(int,input().split()))
 
-# 각 위치에서 왼쪽으로의 최대 높이를 저장하는 리스트
-left_max = [0] * W
-left_max[0] = blocks[0]
-for i in range(1, W):
-    left_max[i] = max(left_max[i-1], blocks[i])
-
-# 각 위치에서 오른쪽으로의 최대 높이를 저장하는 리스트
-right_max = [0] * W
-right_max[-1] = blocks[-1]
-for i in range(W-2, -1, -1):
-    right_max[i] = max(right_max[i+1], blocks[i])
-
-water = 0
-for i in range(W):
-    water += min(left_max[i], right_max[i]) - blocks[i]
-
-print(water)
+a=[0]*(m)
+a[0]=x[0]
+for i in range(1,m):
+    a[i]=max(a[i-1], x[i])
+    
+b=[0]*(m)
+b[-1]=x[-1]
+for i in range(m-2,-1,-1):
+    b[i]=max(b[i+1],x[i])
+    
+result=0
+for i in range(m):
+    result+=min(a[i],b[i])-x[i]
+    
+print(result)

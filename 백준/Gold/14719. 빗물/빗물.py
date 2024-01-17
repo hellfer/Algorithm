62870f392f7e3f201710dac0c1aac86f0cddcba8
JEONG JU <147736877+hellfer@users.noreply.github.com>
2024-01-17 19:01:51 +0900

    [Gold V] Title: 빗물, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Gold/14719. 빗물/빗물.py
+++ 백준/Gold/14719. 빗물/빗물.py
@@ -1,12 +1,12 @@
 n,m=map(int,input().split())
 x=list(map(int,input().split()))
 
-a=[0]*(m)
+a=[0]*m
 a[0]=x[0]
 for i in range(1,m):
-    a[i]=max(a[i-1], x[i])
+    a[i]=max(a[i-1],x[i])
     
-b=[0]*(m)
+b=[0]*m
 b[-1]=x[-1]
 for i in range(m-2,-1,-1):
     b[i]=max(b[i+1],x[i])

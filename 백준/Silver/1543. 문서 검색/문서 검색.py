b6df7560a41d2a14556c92b1231938ef67927481
JEONG JU <147736877+hellfer@users.noreply.github.com>
2024-01-19 15:00:58 +0900

    [Silver V] Title: 문서 검색, Time: 116 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Silver/1543. 문서 검색/문서 검색.py
+++ 백준/Silver/1543. 문서 검색/문서 검색.py
@@ -1,14 +1,14 @@
 x=input()
 y=input()
-count=0
-result=0
+a=0
+b=0
 
-while len(x)-count>=len(y):
-    if x[count:count+len(y)]==y:
-        result+=1
-        count+=len(y)
+while len(x)-a>=len(y):
+    if x[a:a+len(y)]==y:
+        b+=1
+        a+=len(y)
     else:
-        count+=1
+        a+=1
         
-print(result)
-
+print(b)
+        

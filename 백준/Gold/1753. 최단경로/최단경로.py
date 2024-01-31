e2672c2561600e0f658e823075c9f6d5ad30bd94
JEONG JU <147736877+hellfer@users.noreply.github.com>
2024-01-31 22:57:55 +0900

    [Gold IV] Title: 최단경로, Time: 484 ms, Memory: 135588 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.py
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -2,30 +2,35 @@ import sys
 import heapq
 input=sys.stdin.readline
 
-n,m=map(int,input().split())
 INF=float('inf')
+
+n,m=map(int,input().split())
 graph=[[] for _ in range(n+1)]
 distance=[INF]*(n+1)
+visited=[False]*(n+1)
 start=int(input())
+
 for _ in range(m):
     x,y,z=map(int,input().split())
     graph[x].append((y,z))
     
 def dijkstra(start):
     queue=[]
-    heapq.heappush(queue,(0,start))
     distance[start]=0
+    visited[start]=True
+    heapq.heappush(queue,(0,start))
     while queue:
         a,b=heapq.heappop(queue)
         if distance[b]<a:
             continue
         for i in graph[b]:
             cost=a+i[1]
-            if cost<distance[i[0]]:
+            if distance[i[0]]>cost:
                 distance[i[0]]=cost
                 heapq.heappush(queue,(cost,i[0]))
-   
+                
 dijkstra(start)
+
 for i in range(1,n+1):
     if distance[i]==INF:
         print('INF')

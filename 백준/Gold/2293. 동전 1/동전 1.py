b7bd55282df14906e96d1979cf9919bd64cb5897
JEONG JU <147736877+hellfer@users.noreply.github.com>
2024-02-20 20:14:01 +0900

    [Gold V] Title: 동전 1, Time: 136 ms, Memory: 111512 KB -BaekjoonHub

---
+++ 백준/Gold/2293. 동전 1/동전 1.py
@@ -0,0 +1,10 @@
+n, k = map(int, input().split())
+dp = [0 for _ in range(k+1)]
+dp[0] = 1
+
+for _ in range(n):
+    coin = int(input())
+    for i in range(coin, k+1):
+        dp[i] += dp[i - coin]
+
+print(dp[k])

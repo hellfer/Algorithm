df9230f05b49613ab2699c237cce069453015c91
JEONG JU <147736877+hellfer@users.noreply.github.com>
2024-03-04 22:47:55 +0900

    [Bronze V] Title: SUAPC 2023 Summer, Time: 112 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/31429. SUAPC 2023 Summer/SUAPC 2023 Summer.py
@@ -0,0 +1,24 @@
+n=int(input())
+
+if n==1:
+    print(f'{12} {1600}')
+if n==2:
+    print(f'{11} {894}')
+if n==3:
+    print(f'{11} {1327}')
+if n==4:
+    print(f'{10} {1311}')
+if n==5:
+    print(f'{9} {1004}')
+if n==6:
+    print(f'{9} {1178}')
+if n==7:
+    print(f'{9} {1357}')
+if n==8:
+    print(f'{8} {837}')
+if n==9:
+    print(f'{7} {1055}')
+if n==10:
+    print(f'{6} {556}')
+if n==11:
+    print(f'{6} {773}')     

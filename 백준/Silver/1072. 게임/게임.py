8c78bf36cceaf5671f139a738b4e06cfe7bc4719
JEONG JU <147736877+hellfer@users.noreply.github.com>
2024-05-19 23:35:11 +0900

    [Silver III] Title: 게임, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1072. 게임/게임.py
+++ 백준/Silver/1072. 게임/게임.py
@@ -2,18 +2,17 @@ x,y=map(int,input().split())
 
 z=(y*100)//x
 
+result=0
+left,right=0,x
 if z>=99:
     print(-1)
 else:
-    left,right=0,x
-    result=-1
     while left<=right:
         mid=(left+right)//2
-        a=((y+mid)*100)//(x+mid)
+        a=((y+mid)*100 // (x+mid))
         if a>z:
             result=mid
             right=mid-1
         else:
             left=mid+1
-            
     print(result)
